67f80d27f3f8e2b4a1d0e656afa189b6add6617a
Alisdair Hamblyn <ahamblyn@gmail.com>
2018-06-21 10:31:16 +1200

    D1.7: Pass target instance to page.

--- wct-core/src/main/java/org/webcurator/ui/profiles/controller/H3ScriptConsoleController.java
+++ wct-core/src/main/java/org/webcurator/ui/profiles/controller/H3ScriptConsoleController.java
@@ -19,10 +19,9 @@ import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractCommandController;
 import org.webcurator.auth.AuthorityManager;
-import org.webcurator.core.profiles.ProfileManager;
 import org.webcurator.core.scheduler.TargetInstanceManager;
 import org.webcurator.domain.model.auth.Privilege;
-import org.webcurator.domain.model.core.Profile;
+import org.webcurator.domain.model.core.TargetInstance;
 import org.webcurator.ui.common.CommonViews;
 import org.webcurator.ui.profiles.command.H3ScriptConsoleCommand;
 
@@ -54,11 +53,11 @@ public class H3ScriptConsoleController extends AbstractCommandController {
 	@Override
 	protected ModelAndView handle(HttpServletRequest req, HttpServletResponse res, Object comm, BindException errors) throws Exception {
 		H3ScriptConsoleCommand command = (H3ScriptConsoleCommand) comm;
-		Profile profile = targetInstanceManager.getTargetInstance(command.getTargetInstanceOid(), true).getProfile();
+		TargetInstance ti = targetInstanceManager.getTargetInstance(command.getTargetInstanceOid(), true);
 
-		if (authorityManager.hasAtLeastOnePrivilege(profile, new String[] {Privilege.MANAGE_TARGET_INSTANCES, Privilege.MANAGE_WEB_HARVESTER})) {
+		if (authorityManager.hasAtLeastOnePrivilege(ti.getProfile(), new String[] {Privilege.MANAGE_TARGET_INSTANCES, Privilege.MANAGE_WEB_HARVESTER})) {
 			ModelAndView mav = new ModelAndView("h3-script-console");
-			mav.addObject("profile", profile);
+			mav.addObject("targetInstance", ti);
 			return mav;
 		}
 		else { 
--- wct-core/src/main/webapp/jsp/h3-script-console.jsp
+++ wct-core/src/main/webapp/jsp/h3-script-console.jsp
@@ -4,27 +4,27 @@
 <table>
   <tr>
     <td class="subBoxTextHdr">Name</td>
-    <td class="subBoxText"><c:out value="${profile.name}"/></td>
+    <td class="subBoxText"><c:out value="${targetInstance.profile.name}"/></td>
   </tr>
   <tr>
     <td class="subBoxTextHdr">Description</td>
-    <td class="subBoxText"><c:out value="${profile.description}"/></td>
+    <td class="subBoxText"><c:out value="${targetInstance.profile.description}"/></td>
   </tr>
   <tr>
     <td class="subBoxTextHdr">Agency</td>
-    <td class="subBoxText"><c:out value="${profile.owningAgency.name}"/></td>
+    <td class="subBoxText"><c:out value="${targetInstance.profile.owningAgency.name}"/></td>
   </tr>      
   <tr>
     <td class="subBoxTextHdr">State</td>
-    <td class="subBoxText"><spring:message code="profile.state_${profile.status}"/></td>
+    <td class="subBoxText"><spring:message code="profile.state_${targetInstance.profile.status}"/></td>
   </tr>      
   <tr>
     <td class="subBoxTextHdr">Level</td>
-    <td class="subBoxText"><c:out value="${profile.requiredLevel}"/></td>
+    <td class="subBoxText"><c:out value="${targetInstance.profile.requiredLevel}"/></td>
   </tr>
 </table>    
 
 
 <div style="height: 400px; width: 710px; overflow: scroll; border: 1px solid black; padding: 10px;">
-  <pre><c:out value="${profile.profile}"/></pre>
+  <pre><c:out value="${targetInstance.profile.profile}"/></pre>
 </div>
