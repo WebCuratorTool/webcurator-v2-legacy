66f816bd304a5f7eec0033fdaaa70390af2f47fb
Hanna Koppelaar <hanna.koppelaar@kb.nl>
2019-01-09 11:07:10 +0100

    82: Fixed errors in db upgrade scripts

--- wct-core/db/legacy/upgrade/upgrade-postgres-1_5_1-to-1_5_2.sql
+++ wct-core/db/legacy/upgrade/upgrade-postgres-1_5_1-to-1_5_2.sql
@@ -25,7 +25,7 @@ ALTER TABLE DB_WCT.TARGET_INSTANCE_ORIG_SEED ALTER COLUMN TIOS_SEED TYPE varchar
 
 -- The following query is optional and may be used to update the database of targets that are incorrectly set to complete after harvesting.
 -- This is a bug arisig in release 1.5.1 and is fixed in the next version. Run at your own peril!
-UPDATE abstract_target at SET at_state = 5 WHERE at_state = 7 
+UPDATE DB_WCT.ABSTRACT_TARGET at SET at_state = 5 WHERE at_state = 7
 AND EXISTS(SELECT s_oid FROM schedule WHERE s_target_id = at.at_oid AND (s_end IS NULL OR s_end > now()));
 
  
